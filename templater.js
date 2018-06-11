322eb72f48e0659b5ec240088b9cf5e751f72b20
Ruslan Arshinnik <rarshinnik@justcoded.co>
2018-06-11 13:44:39 +0300

    stage 3

--- templater.js
+++ templater.js
@@ -11,11 +11,30 @@ const Templater = {
       });
   },
 
+  render: function (template, element) {
+    const regexp = /{{(.*?)}}/g;
+    let params;
+
+    while ((params = regexp.exec(template)) !== null) {
+      const isHtml = params[1] === 'html',
+            htmlText = element.innerHTML.trim(),
+            defaultText = 'Some text';
+
+      if (isHtml) {
+        template = template.replace(params[0], htmlText.length ? htmlText : defaultText);
+      } else {
+        template = template.replace(params[0], element.getAttribute(params[1]));
+      }
+    }
+
+    return template;
+  },
+
   run: function () {
     this.components.forEach(component => {
       const element = document.querySelector(component.tag);
-      
-      element.outerHTML = component.template;
+
+      element.outerHTML = this.render(component.template, element);
     });
   }
 };
